817488aeba7e404d4bc1ce3a9ad113894f182c23
Rupsha <rupsha.bagchi@gmail.com>
2017-02-28 15:07:49 +0200

    Delete killnode.sh

--- geth_startup_scripts/killnode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-NODE_NAME=$1
-NODE_NAME=${NODE_NAME:-"node1"}
-CONTAINER_NAME="ethereum-$NODE_NAME"
-echo "Destroying container $CONTAINER_NAME..."
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-
